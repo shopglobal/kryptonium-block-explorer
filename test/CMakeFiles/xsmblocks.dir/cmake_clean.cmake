c70d6ecadedbd1afd1cb1cec0797ae872d1fbbab
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-14 01:26:45 +0000

    initials

---
+++ test/CMakeFiles/xsmblocks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xsmblocks.dir/main.cpp.o"
+  "xsmblocks.pdb"
+  "xsmblocks"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xsmblocks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
